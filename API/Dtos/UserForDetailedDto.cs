a98cac68b38fdee59071b0a915a526d4d023d8ce
Ian Marrs <ian.t.marrs@gmail.com>
2020-11-16 11:57:50 -0700

    added UserForListDto and added UserForDetailedDto

---
+++ API/Dtos/UserForDetailedDto.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class UserForDetailedDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string GamerHandle { get; set; }
+        public string Platforms { get; set; }
+        public string Games { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Intro { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }
+}
