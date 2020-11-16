0ae03501bfaed7582f839e0d0c98d9e14c65f294
Ian Marrs <ian.t.marrs@gmail.com>
2020-11-16 15:04:02 -0700

    fixed PhotoUrl data with correct path and used auto mapper to return details back to the browser

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -12,8 +12,21 @@ namespace API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailedDto>();
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p =>
+                        p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p =>
+                        p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt =>
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
+            CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
 }
