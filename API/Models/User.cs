9d9ef8edd596865c1388c3435190cac8dca3d976
Ian Marrs <ian.t.marrs@gmail.com>
2020-11-15 19:08:12 -0700

    Added new migration that expanded the database with more user info and a photos table. Seeded database with dummy data.

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -11,5 +11,18 @@ namespace API.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string GamerHandle { get; set; }
+        public string Platforms { get; set; }
+        public string Games { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Intro { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
